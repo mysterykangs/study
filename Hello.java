49e2e67df00de2cddb3e9938188d2b2270ce1cf5
Kangk <84064246+mysterykangs@users.noreply.github.com>
2021-05-13 11:52:13 +0900

    두번째 버전
    인사 두번

--- Hello.java
+++ Hello.java
@@ -2,8 +2,9 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
         System.out.println("Hello java");
+	System.out.println("Hi");
 
 	}
 
